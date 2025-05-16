baf4df3733377fad4e8e345a72adcbf5a4d5831e
HenriqueCO17 <ohenriquecunha@email.com>
2025-05-16 00:26:15 -0300

    Criando a injeção de dependência e a String de conexão

--- Projeto4/Program.cs
+++ Projeto4/Program.cs
@@ -1,7 +1,13 @@
+using Projeto4.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<LoginRepositorio>();
+builder.Services.AddScoped<ProdutoRepositorio>();
+
 
 var app = builder.Build();
 
