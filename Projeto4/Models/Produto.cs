2311f1c43cb94dff700a71d89af9516f1be9e05d
HenriqueCO17 <ohenriquecunha@email.com>
2025-05-15 23:46:47 -0300

    Criamdo as Models Produto e Usuario

---
+++ Projeto4/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto4.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string ?Nome { get; set; }
+        public string ?Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public List<Produto> ListaProduto { get; set; }
+    }
+}
