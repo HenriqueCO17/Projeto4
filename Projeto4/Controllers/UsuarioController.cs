91b2658be9e464b5e6c03b066509c44bceb6fa80
HenriqueCO17 <ohenriquecunha@email.com>
2025-05-16 00:12:11 -0300

    Criando as Controlers Usuario e Produto

---
+++ Projeto4/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto4.Repositorio;
+
+namespace Projeto4.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly LoginRepositorio _loginRepositorio;
+
+        public UsuarioController(LoginRepositorio loginRepositorio)
+        {
+            _loginRepositorio = loginRepositorio;
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _loginRepositorio.ObterUsuario(email);
+
+            if (usuario != null && usuario.Senha == senha)
+            {
+                return RedirectToAction("Index", "Produto");
+            }
+
+            ModelState.AddModelError("", "Email ou senha inválidos. ");
+
+            return View();
+        }
+    }
+}
